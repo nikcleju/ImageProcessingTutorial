471528bbe2bcc16e133f65d55b3355cbc81678ee
Nicolae Cleju <nikcleju@gmail.com>
2022-09-15 16:51:40 +0300

    Convert to ipynb

---
+++ to_notebooks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+jupytext --to notebook GeometricTransformations.qmd
+jupytext --to notebook LinearFiltering.qmd
+jupytext --to notebook Binarization.qmd
+jupytext --to notebook MorphologicalOperations.qmd
